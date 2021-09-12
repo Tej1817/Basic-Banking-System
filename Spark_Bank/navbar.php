4ed5eb1ac538bcdb36a252f6c63a7e0c28cd725b
Tej1817 <90557828+Tej1817@users.noreply.github.com>
2021-09-12 19:38:01 +0530

    Add files via upload

---
+++ Spark_Bank/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light" >
+      <a class="navbar-brand" href="index.php">THE SPARKS FOUNDATION BANK <i class="fas fa-university"></i></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><i class="fas fa-home"></i>Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createusers.php"><i class="far fa-user"></i>Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><i class="fas fa-comments-dollar"></i>Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><i class="fas fa-history"></i>Transaction History</a>
+              </li>
+          </div>
+       </nav>
